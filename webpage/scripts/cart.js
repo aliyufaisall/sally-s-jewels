20fb969fda7d2fb4c92360d2d190b10eac36fc93
Aliyu Abubakar <aleeyu011@gmail.com>
2026-02-28 12:56:18 +0100

    Refactored cart.js code

--- webpage/scripts/cart.js
+++ webpage/scripts/cart.js
@@ -1,1 +1,37 @@
-cart = [];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+export function updateCartCount() {
+
+  const cartCount =
+    document.querySelector(".cart-count");
+
+  if (!cartCount) return;
+
+  let total = 0;
+
+  cart.forEach(item => {
+    total += item.quantity;
+  });
+
+  cartCount.textContent = total;
+}
+
+export function addToCart(productId, quantity) {
+  let matchingItem;
+
+  cart.forEach(function(item) {
+    if (item.id === productId) {
+      matchingItem = item;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      id: productId, 
+      quantity: quantity
+    });
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
